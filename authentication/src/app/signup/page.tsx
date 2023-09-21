b8fad32862ee7f1cd53e68ce8793d4a534bad503
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-09-21 19:38:16 +0530

    Created Signup functionality successfully and and sent data to database.

--- authentication/src/app/signup/page.tsx
+++ authentication/src/app/signup/page.tsx
@@ -2,11 +2,11 @@
 
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { axios } from "axios";
+import { useRouter } from 'next/navigation'
+import axios from "axios";
 
 function SignupPage() {
-    //const router = useRouter();
+    const router = useRouter();
     const [user, setUser] = useState({
         username: "",
         email: "",
@@ -14,8 +14,21 @@ function SignupPage() {
     });
 
     const [buttonDisabled, setButtonDisabled] = useState(false);
-    const onSignup = async () => {
 
+    const [loading, setLoading] = useState(false);
+
+    const onSignup = async () => {
+        try {
+            setLoading(true);
+            const response = axios.post("/api/users/signup", user);
+            console.log("Signup Successfull", response.data);
+
+            router.push("/login");
+        } catch (error: any) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
     }
 
     useEffect(() => {
@@ -29,6 +42,7 @@ function SignupPage() {
 
     return (
         <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+            <h1>{loading ? "Processing" : "Signup"}</h1>
             <h1 className='text-center text-white text-2xl'>
                 Signup
             </h1>
