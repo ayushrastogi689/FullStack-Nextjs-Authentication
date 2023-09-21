830bfe403b32e8505d12db72c7cfaa359377c5a5
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-09-21 19:42:52 +0530

    Created Login functionality.

--- authentication/src/app/login/page.tsx
+++ authentication/src/app/login/page.tsx
@@ -2,18 +2,40 @@
 
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { axios } from "axios";
+import { useRouter } from 'next/navigation'
+import axios from "axios";
 
 
 function LoginPage() {
+    const router = useRouter();
     const [user, setUser] = useState({
         email: "",
         password: "",
     });
-    const onLogin = async () => {
 
+    const [buttonDisabled, setButtonDisabled] = useState(false);
+    const [loading, setLoading] = useState(false)
+
+    const onLogin = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/login", user);
+            console.log("Login Successfull", response.data);
+            router.push("/profile");
+        } catch (error: any) {
+            console.log("Login failed " + error.message);
+        } finally {
+            setLoading(false);
+        }
     }
+
+    useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+    }, [user])
 
     return (
         <div className='flex flex-col items-center justify-center min-h-screen py-2'>
